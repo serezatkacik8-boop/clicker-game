2a7453a108d53d00462dcbfdf37c978ff0c8e7cb
Serhii <serezatkacik8@gmail.com>
2026-04-13 20:51:42 +0300

    savedGame

--- script.js
+++ script.js
@@ -1,50 +1,46 @@
+
 let score = 0;
-
 let power = 1;
-
 let autoClickers = 0;
 
+
+
 const scoreEl = document.getElementById('score');
-
 const powerEl = document.getElementById('power');
-
 const clickBtn = document.getElementById('clickBtn');
-
 const upgradeBtn = document.getElementById('upgradeBtn');
 const autoBtn = document.getElementById('autoBtn');
-
 const messageEl = document.getElementById('message');
+
+
 
 function updateScreen() {
   scoreEl.textContent = score;
   powerEl.textContent = power;
 }
-function showMessage(text){
+
+function showMessage(text) {
   messageEl.textContent = text;
 }
-function updateScreen(){
-  scoreEl.textContent = score;
-}
-function updateScreen(){
-  scoreEl.textContent = score;
-  powerEl.textContent = power;
-}
-function showMessage(text){
-  messageEl.textContent = text;
-}
+
+
+
 function saveGame() {
   localStorage.setItem('score', score);
   localStorage.setItem('power', power);
   localStorage.setItem('autoClickers', autoClickers);
 }
+
 function loadGame() {
   const savedScore = localStorage.getItem('score');
-  const savedPover = localStorage.getItem('power');
+  const savedPower = localStorage.getItem('power'); // исправлено
   const savedAutoClickers = localStorage.getItem('autoClickers');
-  if(savedScore !== null){
+
+  if (savedScore !== null) {
     score = Number(savedScore);
   }
-    if (savedPower !== null) {
+
+  if (savedPower !== null) {
     power = Number(savedPower);
   }
 
@@ -52,42 +48,54 @@ function loadGame() {
     autoClickers = Number(savedAutoClickers);
   }
 }
+
+
 clickBtn.addEventListener('click', () => {
   score += power;
   updateScreen();
+  saveGame(); 
 });
+
+
 upgradeBtn.addEventListener('click', () => {
-  if (score >= 10){
+  if (score >= 10) {
     score -= 10;
     power += 1;
+
     updateScreen();
     showMessage('Клик улучшен!');
-  }
-  else{
+    saveGame(); 
+  } else {
     showMessage('Не хватает очков');
   }
 });
+
+
+
 autoBtn.addEventListener('click', () => {
-  if( score >= 50){
-    score -=50;
+  if (score >= 50) {
+    score -= 50;
     autoClickers += 1;
+
     updateScreen();
     showMessage('Авто-кликер куплен!');
-  }
-  else{
+    saveGame(); 
+  } else {
     showMessage('Не хватает очков');
   }
 });
+
+
+
 setInterval(() => {
-
   if (autoClickers > 0) {
-
     for (let i = 0; i < autoClickers; i++) {
       clickBtn.click();
     }
-
   }
-
 }, 1000);
-loadGame();
-updateScreen();
+
+
+
+loadGame();  
+updateScreen(); 
