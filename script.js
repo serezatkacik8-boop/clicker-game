3d532828e13734912b0029cafb2406c3bdef578f
Serhii <serezatkacik8@gmail.com>
2026-04-13 20:07:58 +0300

    update

--- script.js
+++ script.js
@@ -22,3 +22,39 @@ function updateScreen() {
 function showMessage(text){
   messageEl.textContent = text;
 }
+function updateScreen(){
+  scoreEl.textContent = score;
+}
+function updateScreen(){
+  scoreEl.textContent = score;
+  powerEl.textContent = power;
+}
+function showMessage(text){
+  messageEl.textContent = text;
+}
+clickBtn.addEventListener('clickck', () => {
+  score += power;
+  updateScreen();
+});
+upgradeBtn.addEventListener('click', () => {
+  if (score >= 10){
+    score -= 10;
+    power += 1;
+    updateScreen();
+    showMessage('Клик улучшен!');
+  }
+  else{
+    showMessage('Не хватает очков');
+  }
+});
+autoBtn.addEventListener('click', () => {
+  if( score >= 50){
+    score -=50;
+    autoClickers += 1;
+    updateScreen();
+    showMessage('Авто-кликер куплен!');
+  }
+  else{
+    showMessage('Не хватает очков');
+  }
+});
